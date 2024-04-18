769bf3d4d2c05c1d5b568c356168ddd261a93b64
Arka102202 <arka.online1995@gmail.com>
2024-04-18 20:49:22 +0530

    search box done

--- src/api func/fetchFunc.js
+++ src/api func/fetchFunc.js
@@ -2,7 +2,7 @@ const fetchDataUsingAPI = async (url, options) => {
 
   try {
     const resp = await fetch(url, options);
-    if(!resp.ok) throw new Error("");
+    if (!resp.ok) throw new Error("");
     const result = await resp.json();
     if (!result.error) {
       return result;
@@ -15,3 +15,22 @@ const fetchDataUsingAPI = async (url, options) => {
     return { error: true, message: "Something went wrong..." };
   }
 };
+
+export const getPlaceInfo = async (query = "") => {
+  const url = `https://trueway-places.p.rapidapi.com/FindPlaceByText?text=${encodeURIComponent(query)}&language=en`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key':  process.env.REACT_APP_Key,
+      'X-RapidAPI-Host': process.env.REACT_APP_location_Host
+    }
+  };
+
+  const resp = await fetchDataUsingAPI(url, options);
+
+  if (resp && resp.results.length === 0) return { error: true, message: "No place found ...." };
+
+  console.log(resp.results[0]);
+
+  return resp.results[0];
+}
--- src/component/searchBox/SearchBox.jsx
+++ src/component/searchBox/SearchBox.jsx
@@ -2,19 +2,32 @@ import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import logo from "../../assets/information-button.png";
 import { Tooltip } from 'react-tooltip';
+import { getPlaceInfo } from '../../api func/fetchFunc';
 
 const SearchBox = () => {
   const [place, setPlace] = useState("");
+
+
+  const fetchPlaceData = async (query = "") => {
+    const placeInfo = await getPlaceInfo(query);
+    setPlace(placeInfo);
+  }
+
 
   return (
     <div className='search-box'>
       <label htmlFor="place">Search for a place :</label>
       <input type="text" id='place' value={place} onChange={e => setPlace(e.target.value)}
-        placeholder='place or city, pincode, state, country' />
+        onKeyDown={e => {
+          if (e.key === "Enter") {
+            fetchPlaceData(e.target.value);
+          }
+        }}
+        placeholder='place or city, pincode, state, country and press enter' />
       <button className='btn-extra-info' data-tooltip-id="my-tooltip" data-tooltip-content='For exact match and precise location enter the full address'>
         <img src={logo} alt="" />
       </button>
-      <Tooltip id='my-tooltip' className='tooltip'/>
+      <Tooltip id='my-tooltip' className='tooltip' />
     </div>
   );
 };
